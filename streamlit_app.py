7afd0f2fbb709fedf1b72f34b3f4558d433c97ea
Boothalingam J S <jsboothalingam90.microsoft@gmail.com>
2024-02-26 13:27:39 +0530

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pypyodbc as pyodbc
 import main
+
+
 
 config = {
             "api_key" : 'AIzaSyAqkMyd-Dp5gB3pxc7OU3TeFgBxaRnQoHY',
@@ -25,6 +28,7 @@ with tab1:
    SearchVideoName = st.text_input('Video Name : ', '')
    SearchDescription = st.text_input('Description : ', '')
    submitSearch = st.button("Search")
+   print(pyodbc.drivers())
    if submitSearch:
     header = ["Video name", "Video description", "Published date", "View count", "Like count", "Channel name"]
     df = pd.DataFrame(mf.getListofChannelDetails("", SearchChannelName, SearchVideoName, SearchDescription), columns=(header))
