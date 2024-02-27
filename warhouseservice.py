2f7553271b40a26e9ed42ac8037eff5966a10697
Boothalingam J S <jsboothalingam90.microsoft@gmail.com>
2024-02-27 15:38:28 +0530

    Debug

--- warhouseservice.py
+++ warhouseservice.py
@@ -21,7 +21,9 @@ class warhousehelper:
         return "video_insert @video_id = ?, @playlist_id = ?, @video_name = ?, @video_description = ?, @published_date = ?, @view_count = ?, @like_count = ?, @dislike_count = ?, @favorite_count = ?, @comment_count = ?, @duration = ?, @thumbnail = ?, @caption_status = ?, @durationtime = ?"
 
     def getvideovalues(self, data):
-        return (data["Video_Id"], data["Playlist_id"], data["Video_Name"], data["Video_Description"], data["PublishedAt"], data["View_Count"], data["Like_Count"], data["Dislike_Count"], data["Favorite_Count"], data["Comment_Count"], data["Duration"], data["Thumbnail"], data["Caption_Status"], data["TimeStamp"])
+        Video_Description = data["Video_Description"]
+        Video_Description = Video_Description[:10]
+        return (data["Video_Id"], data["Playlist_id"], data["Video_Name"], Video_Description, data["PublishedAt"], data["View_Count"], data["Like_Count"], data["Dislike_Count"], data["Favorite_Count"], data["Comment_Count"], data["Duration"], data["Thumbnail"], data["Caption_Status"], data["TimeStamp"])
     
     def insertchannel(self, data):
         self.sqlcon.insert(self.getchannelquery(data), self.getchannelvalues(data))
@@ -38,9 +40,8 @@ class warhousehelper:
         for commentitem in comment_data.keys():
             comment_tem_data = comment_data[commentitem]
             Comment_Text = comment_tem_data["Comment_Text"]
-            Comment_Text = Comment_Text[1:10]
+            Comment_Text = Comment_Text[:10]
             commentvalues = (comment_tem_data["Comment_Id"], comment_tem_data["Video_Id"], Comment_Text, comment_tem_data["Comment_Author"], comment_tem_data["Comment_PublishedAt"], comment_tem_data["Like_count"], comment_tem_data["Reply_count"], comment_tem_data["Comment_key"])
-            st.info(commentvalues)
             self.sqlcon.insert(insert_query, commentvalues)
     
     def insertChannelData(self, data):
